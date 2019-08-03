d1fe461d385aea4a7f5c4934ba79461212719d5a
Travis CI User <travis@example.org>
2019-08-03 13:24:51 +0000

    [ci skip] version changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lightning-talk-pipelines",
-    version="0.0.7",
+    version="0.0.8",
     author="Rory Murdock",
     author_email="rory@itmatic.com.au",
     description="A sample package for testing",
